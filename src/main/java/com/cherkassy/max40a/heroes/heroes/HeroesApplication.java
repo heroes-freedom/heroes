0973a161bdb6ce149478e590b8666e928293965a
Retro <max40a@mail.ru>
2018-03-04 16:44:04 +0200

    -init commit;

---
+++ src/main/java/com/cherkassy/max40a/heroes/heroes/HeroesApplication.java
@@ -0,0 +1,26 @@
+package com.cherkassy.max40a.heroes.heroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class HeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesApplication.class, args);
+	}
+}
+
+@RestController
+@RequestMapping("api/hello")
+class HelloController {
+
+	@GetMapping
+	public Mono<String> hello() {
+		return Mono.just("Hello World");
+	}
+}
